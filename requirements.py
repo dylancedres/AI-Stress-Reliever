9ef7e7cbeb54a9f4e87616d2601b23c8d8c10da8
dylancedres <70666980+dylancedres@users.noreply.github.com>
2025-04-08 16:27:40 -0400

    Create requirements.py

---
+++ requirements.py
@@ -0,0 +1,7 @@
+streamlit        ~= 1.27.0
+numpy            ~= 1.26.0
+pandas           ~= 2.1.0
+plotly           ~= 5.17.0
+seaborn          ~= 0.12.2
+matplotlib       ~= 3.7.0
+# python_version ~= 3.11.4
