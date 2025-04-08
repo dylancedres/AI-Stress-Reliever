270775da1dd5af627ebb18c7cfecc01215f415d9
dylancedres <70666980+dylancedres@users.noreply.github.com>
2025-04-08 16:32:08 -0400

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,4 +1,19 @@
 import pandas as pd, pandasai as pai, streamlit as st, numpy as np
 
+st.set_page_config(layout="wide",
+                   page_title="PR Kidney Disease-SDOH",
+                   page_icon=":test_tube:",
+                   menu_items={"Get help":"mailto:dylan.cedres@upr.edu",
+                               "Report a Bug":"mailto:dylan.cedres@upr.edu",
+                               "About":"Dashboard with Kidney Disease Lab Data and Social Determinants of Health Indices"})
+
 print("Testing the App through the terminal")
 st.write("Testing the App through the web browser")
+
+
+# Removes "Made with Streamlit"
+hide_style = """<style>
+                 footer {visibility: hidden;}
+             </style>"""
+
+st.markdown(hide_style, unsafe_allow_html=True)
