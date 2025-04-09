8fa585744bfebf9c0a7f909ee0eb768bf43eecc5
dylancedres <70666980+dylancedres@users.noreply.github.com>
2025-04-09 12:59:37 -0400

    Update temp.py

--- temp.py
+++ temp.py
@@ -11,6 +11,7 @@ print("Testing the App through the terminal")
 st.subheader("Testing the App through the web browser")
 # st.write("Testing the App through the web browser")
 
+
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -25,6 +26,21 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
+# React to user input
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    st.chat_message("user").markdown(prompt)
+    
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    
+    response = f"Echo: {prompt}"
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
 
 
 # Removes "Made with Streamlit"
