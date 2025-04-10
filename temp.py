a146990934cdc7bda4dc3ca468b9055545646926
dylancedres <70666980+dylancedres@users.noreply.github.com>
2025-04-10 15:10:07 -0400

    Update temp.py

--- temp.py
+++ temp.py
@@ -16,7 +16,7 @@ def greeting_generator():
     
     for word in greeting.split():
         yield word + " "
-        time.sleep(0.1)
+        time.sleep(0.5)
 
 st.set_page_config(layout="wide",
                    page_title="PR Stress Reliever",
